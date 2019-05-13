e024ac9b034e7e3b59e692720907781112b588d2
roman <romka69tver@gmail.com>
2019-05-13 03:46:29 +0300

    add tasks#index, close #1

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -3,5 +3,11 @@ FactoryBot.define do
     title { "MyTitle" }
     body { "MyBody" }
     price { 777555 }
+
+    factory :task_sequence do
+      sequence(:title) { |n| "MyTitle#{n}" }
+      sequence(:body) { |n| "MyBody#{n}" }
+      sequence(:price) { |n| 777 + n }
+    end
   end
 end
