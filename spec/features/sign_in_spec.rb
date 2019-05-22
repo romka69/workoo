6069d603aeaafd5747ffe7bdddf769d04e546312
roman <romka69tver@gmail.com>
2019-05-22 20:37:37 +0300

    test for logout, close #3

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 feature 'The guest can register as customer or executor' do
-
   describe 'Sign in' do
     background do
       visit root_path
@@ -46,24 +45,57 @@ feature 'The guest can register as customer or executor' do
       visit root_path
       click_on 'Регистрация'
 
-      sleep 0.4
+      sleep 0.2
 
       fill_in 'Электронная почта', with: 'usertest@usertest.test'
       fill_in 'Пароль', with: '12345678'
       fill_in 'Пароль еще раз', with: '12345678'
     end
 
-    scenario 'Unregistered customer tries to sign up' do
+    scenario 'unregistered customer tries to sign up' do
       click_on 'Зарегистрироваться'
 
       expect(page).to have_content 'Добро пожаловать! Вы успешно зарегистрировались.'
     end
 
-    scenario 'Unregistered executor tries to sign up' do
+    scenario 'unregistered executor tries to sign up' do
       find(:css, 'label', text: 'Зарегистрироваться как исполнитель').click
       click_on 'Зарегистрироваться'
 
       expect(page).to have_content 'Добро пожаловать! Вы успешно зарегистрировались.'
+    end
+  end
+
+  describe 'Log out' do
+    background do
+      User.create!(email: 'usertest@usertest.test', password: '12345678', role: Role.create!(role_name: 'customer'))
+      User.create!(email: 'usertest2@usertest.test', password: '12345678', role: Role.create!(role_name: 'executor'))
+
+      visit new_user_session_path
+    end
+
+    scenario 'customer tries to log out' do
+      fill_in 'Электронная почта', with: 'usertest@usertest.test'
+      fill_in 'Пароль', with: '12345678'
+      click_on 'Войти'
+
+      sleep 0.2
+
+      click_on 'Выйти'
+
+      expect(page).to have_content 'Выход из системы выполнен.'
+    end
+
+    scenario 'executor tries to log out' do
+      fill_in 'Электронная почта', with: 'usertest2@usertest.test'
+      fill_in 'Пароль', with: '12345678'
+      click_on 'Войти'
+
+      sleep 0.2
+
+      click_on 'Выйти'
+
+      expect(page).to have_content 'Выход из системы выполнен.'
     end
   end
 end
